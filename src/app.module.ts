5848cc4806ab9e64b6a162457546d92e9baf5c73
abicontreras27 <wacr.27@gmail.com>
2024-12-22 00:23:26 -0500

    creando modulo subcategory

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { UsersModule } from './users/users.module';
 import { NotificationModule } from './notification/notification.module';
 import { CategoryModule } from './category/category.module';
 import { TicketModule } from './ticket/ticket.module';
+import { SubCategoryModule } from './sub-category/sub-category.module';
 
 @Module({
-  imports: [UsersModule, NotificationModule, CategoryModule, TicketModule],
+  imports: [UsersModule, NotificationModule, CategoryModule, TicketModule, SubCategoryModule],
   controllers: [],
   providers: [],
 })
