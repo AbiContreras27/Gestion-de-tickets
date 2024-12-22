8e1dca96aea40cd9e768d4cb55aa08d7c165aa96
abicontreras27 <wacr.27@gmail.com>
2024-12-22 00:27:53 -0500

    creando modulo attachment

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { CategoryModule } from './category/category.module';
 import { TicketModule } from './ticket/ticket.module';
 import { SubCategoryModule } from './sub-category/sub-category.module';
 import { StatusLogModule } from './status-log/status-log.module';
+import { AttachmentModule } from './attachment/attachment.module';
 
 @Module({
-  imports: [UsersModule, NotificationModule, CategoryModule, TicketModule, SubCategoryModule, StatusLogModule],
+  imports: [UsersModule, NotificationModule, CategoryModule, TicketModule, SubCategoryModule, StatusLogModule, AttachmentModule],
   controllers: [],
   providers: [],
 })
